6e9d3aceddceae1c657ecef1cca9e4ee0b8d58cf
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2025-11-15 11:19:23 -0500

    feat: add env template config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,5 @@ import { TutoriasModule } from './tutorias/tutorias.module';
 
 @Module({
   imports: [TutoriasModule],
- 
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import { envs } from './config';
 
 async function bootstrap() {
 
   const logger = new Logger('wise-tutorias');
   
   const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
 
-  logger.log(`wise-tutorias esta corriendo en el puerto: ${envs.port}`);
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  await app.listen(port);
+
+  logger.log(`wise-tutorias esta corriendo en el puerto: ${port}`);
 }
 bootstrap();
